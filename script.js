8a04246d98166fd93993fd81810175592fc3c932
Sevinj0124 <Cavadova.24@icloud.com>
2024-03-05 20:11:23 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const telebelerS4 =[ {
+    ad: 'Jale'
+    soyad:'Zuliyeva'
+    tehsili:{
+        ordaMekteb:"63 nomreli mekteb"
+        bakalavriatura:'ADNSU'
+        magistratura:"AzTU"
+        doktorantura:{
+            mudafieIli:'2028'
+            mudafieMovzusu:'IT esaslari'
+        }
+       
+    }
+},
+
+
+{
+    ad: 'Ayxan'
+    soyad:'Mahmudzade'
+    tehsili:{
+        ordaMekteb:"153 nomreli mekteb"
+        bakalavriatura:'Aztu'
+        magistratura:"BANM"
+        doktorantura:{
+            mudafieIli:'2034'
+            mudafieMovzusu:'IT esaslari'
+        }
+      
+    }
+},
+
+]
+
+console.log('MIT Academyde "Front-end uzre" tehsil alan '+ telebelerS4 [0].ad)
